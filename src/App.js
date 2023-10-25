c8c7e5b4b5ecf9941400cf17142d43937341bb6e
Pratham1962 <prathamgawli19@example.com>
2023-10-26 00:27:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import Home from "./Components/Home";
+
+import DataProvider from "./context/DataProvider";
+
+function App() {
+  return (
+      <DataProvider>
+        <Home/>
+      </DataProvider>
+      
+  )
+}
+
+export default App;
